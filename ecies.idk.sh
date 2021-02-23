d98d2d13a8d3edb87ce8fc483b1cf52d4870aebd
idk <hankhill19580@gmail.com>
2021-02-23 12:12:04 -0500

    Hourly push - Tue 23 Feb 2021 12:12:04 PM EST

--- ecies.idk.sh
+++ ecies.idk.sh
@@ -2,7 +2,7 @@
 
 while true; do
   DATE=$(date)
-  mkdir -p $(date +%H) && git add . && git commit -am "Hourly push - $DATE" && git push origin master
+  mkdir -p $(date +%H) && git add . && git commit -am "Hourly push - $DATE" && git push origin main
   time=$(./curl.sh http://idk.i2p/)
   echo $(date +%H%M): $time | tee -a $(date +%H)/ecies.idk.md
   sleep 4m
